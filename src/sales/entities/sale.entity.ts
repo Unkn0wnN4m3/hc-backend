aa3c2b6c416a36515a43df9408500d9ae638e77c
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2023-06-09 16:19:32 -0600

    feat: client entity was added

--- src/sales/entities/sale.entity.ts
+++ src/sales/entities/sale.entity.ts
@@ -1,1 +1,13 @@
-export class Sale {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Sale {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  day: number;
+
+  @Column()
+  hour: number;
+}
