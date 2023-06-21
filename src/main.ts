4b631f423778ee858ed5f82b69e6282d26ae6a48
Joel Rondinel Pacheco <joel.rondinel.pacheco@gmail.com>
2023-06-20 23:13:16 -0300

    CORS agregado, SIN CREDENCIALES

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { CORS } from './cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
 
+  app.enableCors(CORS);
+
   app.useGlobalPipes(new ValidationPipe());
   app.enableVersioning({
     type: VersioningType.URI,
   });
+
   await app.listen(configService.get('PORT'));
 }
 
